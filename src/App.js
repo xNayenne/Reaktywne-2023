e2ba918c8e238718d3fddecaa1839fac4f03ce5d
Magdalena Kłósek <35215@student.pwsztar.edu.pl>
2023-05-04 19:33:16 +0200

    lab7

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizComponent from "./components/quiz-component";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+ return (
+   <QuizComponent/>
+ );
+}
+
+export default App;
+
+
