968d541c95abd3a29cb4a6705205104b0a230971
Magdalena Kłósek <35215@student.pwsztar.edu.pl>
2023-05-07 13:28:08 +0200

    lab8

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Posts from "./components/posts";
+
+function App() {
+  return (
+      <main className="container">
+        <Posts/>
+      </main>
+  );
+}
+
+export default App;
